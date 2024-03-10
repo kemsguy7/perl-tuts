98b02a7ed2127dd237ee7379f25de29718296c64
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-10 11:14:26 +0100

    added regex

--- hello_world.pl
+++ hello_world.pl
@@ -39,15 +39,15 @@ my @sorted = sort @animals;
 print @sorted;
 
 #hashes (associative arrays[key : value pairs])
-my %fruit_color  = ( 
+my %fruit_color  = (
     apple => "red",
     banana => "yellow",
-)
+);
 
 my @fruits = keys %fruit_color;
 my @colors = values %fruit_color;
 
-print @fruits;
+print @fruits . "\n";
 print sort @colors . "\n";
 
 #nesting list and hashes
@@ -58,21 +58,48 @@ my $variables =  {
         },
           array => { 
             description => "ordered list of items",
-            sigil => '@'
+            sigil => '@',
           },
           hash =>  {
             description => "key/value pairs",                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-            sigil = '%',
-          }
-         
+            sigil => '%',
+          },
 
-}
+};
 
+print "Scalars begin with a $variables->{'scalar'}->{'sigil'}\n";
+
+
+#if statements The traditional way
+my $zippy = 0;
+# if ($zippy) {
+#   print "Wow!"; 
+# }
+
+
+#if statments the perlish post conditional way
+my $apples = "";
+print "Wow! wow!" if $zippy;
+print "We have no apples" unless $apples; 
+
+my @array = (1, 2, 3, 4, 5);
 #using foreach
 foreach (@array) {
-    print "This element is $_ \n";
-    
+    print "This element is $_\n";
 }
+ 
+# you don't have to use the default $_ either...
 
 #working with files 
 open (my $in, "<", "input.txt") or die "Can't open input.txt: $!";
+
+#REGULAR EXPRESSIONS
+# # simple matching 
+# if (/foo/)       { ... }  # true if $_ contains "foo"
+# if ($a =~ /foo/) { ... }  # true if $a contains "foo"
+
+
+# s/foo/bar/;               # replaces foo with bar in $_
+# $a =~ s/foo/bar/;         # replaces foo with bar in $a
+# $a =~ s/foo/bar/g;        # replaces ALL INSTANCES of foo with bar
+#                           # in $a
