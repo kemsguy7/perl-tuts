6b383580241595bfee05882882f3bf9bc672cc57
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-10 18:36:36 +0100

    sequential arrays

--- second.pl
+++ second.pl
@@ -11,9 +11,21 @@ print "\$data{'Kumar'} = $data{'Kumar'}\n";
 
 @names = ('John Paul', 'Lisa', 'Kumar'); 
 
-@copy = @names;
-$size = @names; 
+@copy = @names;   #copying an array
+$size = @names;   #copying the size of an array
 
 print "Given names are : @copy\n";
 print "Number of names are : $size\n";
  
+# Array variables can be populated using parentheses and the qw operator
+@days = qw/Mon Tue Wed Thu Fri Sat Sun/;
+@months = ('Jan', 'Feb', 'March');
+
+print "Day: $days[0] in the first month of $months[1] \n";
+
+#sequential number arrays
+@var_10 = (1..10);
+@var_20 = (10..20);
+@var_abc = (a..z);
+
+print "@var_10\n";
