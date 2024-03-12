cd5bc9defaf08313a12df87ac705fe7788e2aa0c
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-12 23:44:02 +0100

    updates

--- second.pl
+++ second.pl
@@ -89,3 +89,10 @@ $[ = 1;
 
 print "Animals at \@animals[1]: $animals[1]\n";
 print "Animals at \@animals[2]: $animals[2]\n";
+
+#selecting elements from Lists
+$var = (5,4,3,2,1)[4];
+print "value of var = $var\n";
+
+@list = (5,4,3,2,1)[1..3];
+print "value of list = @list\n";
