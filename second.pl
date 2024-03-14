aef8bff86c7357201ea8ea2b68d5ecb25346fbde
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-14 13:17:07 +0100

    copying a file

--- second.pl
+++ second.pl
@@ -218,3 +218,19 @@ open(DATA, "<file.txt") or die "Couldn't open file file.txt, $!";
 while(<DATA>) { 
     print "$_";
 }
+
+# COPYING FILES
+#open file to read
+open(DATA1, "<file1/txt");
+
+#Open new file to write 
+open(DATA2, ">file2.txt");
+
+
+# Copy data from one file to another. 
+while(<DATA1>) { 
+    print DATA2 $_;
+}
+
+close ( DATA1 );
+close ( DATA2 );
