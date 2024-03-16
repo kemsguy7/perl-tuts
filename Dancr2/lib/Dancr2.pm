a5c86c878c78fb12564457c4f06c4be94239068c
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-16 08:50:32 +0100

    bug fixing and updates

--- Dancr2/lib/Dancr2.pm
+++ Dancr2/lib/Dancr2.pm
@@ -1,133 +1,93 @@
-#!/usr/bin/env/perl
 package Dancr2;
 use Dancer2;
-
+use Dancer2::Plugin::Database;
+ 
 our $VERSION = '0.1';
-
-use DBI;
-use File::Spec;
-use File::Slurper qw/ read_text /;
-use Template;
  
-#configurations and sessions 
-set 'database'        => File::Spec->catfile(File::Spec->tmpdir(), 'dancr.db');
-set 'session'         => 'YAML';
-set 'template'        => 'template_toolkit';
-set 'logger'          => 'console';
-set 'log'             => 'debug';
-set 'show_stacktrace' => 1;
-set 'startup_info'    => 1;
-
-#custome settings
-set layout => 'main';  #Tells dancer2's template engine tha that it should look for a file called main.tt in views/layouts/ 
-set 'username' => 'admin';
-set 'password' => 'password';
+my $flash;
  
-sub set_flash { 
-    my $message = shift; 
-
-    session flash => $message;
+sub set_flash {
+    my $message = shift;
+ 
+    $flash = $message;
 }
-
+ 
 sub get_flash {
-    my $msg = session('flash');
-    session->delete('flash'); 
-
+    my $msg = $flash;
+    $flash  = "";
+ 
     return $msg;
 }
-
-#connect to database
-sub connect_db {
-    my $dbh = DBI->connect("dbi:SQLite:dbname=".setting('database')) 
-        or die $DBI::errstr;
-
-    return $dbh;
-};
-
-#initialize database
-sub init_db {
-    my $db = connect_db();
-    my $schema = read_text('./schema.sql');
-    $db->do($schema)
-        or die $db->errstr;
-}
-
+ 
 hook before_template_render => sub {
-    my $tokens = shift; 
-
-    $tokens->{'css_url'}  = request->base . 'css/style.css';
-    $tokens->{'login_url'} = uri_for('/login');
+    my $tokens = shift;
+ 
+    $tokens->{'css_url'}    = request->base . 'css/style.css';
+    $tokens->{'login_url'}  = uri_for('/login');
     $tokens->{'logout_url'} = uri_for('/logout');
 };
-
-#Route Handlers 
-get '/' => sub { 
-    my $db  = connect_db();
-    my $sql = 'select, id, title, text from entries order by id desc';
-
-    my $sth = $db->prepare($sql)
-        or die $db->errstr;
-    
-    $sth->execute 
-        or die $sth->errstr;
-
-    template 'show_entries.tt', {
-        msg  => get_flash(),
-        add_entry_url => uri_for('/add'),
-        entries => $sth->fetchall_hashref('id'),
-    };  
+ 
+hook 'database_error' => sub {
+    my $error = shift;
+    die $error;
 };
-
-
-#add post route 
+ 
+get '/' => sub {
+    my $sql = 'select id, title, text from entries order by id desc';
+    my $sth = database->prepare($sql);
+    $sth->execute;
+    template 'show_entries.tt', {
+        msg           => get_flash(),
+        add_entry_url => uri_for('/add'),
+        entries       => $sth->fetchall_hashref('id'),
+    };
+};
+ 
 post '/add' => sub {
     if ( not session('logged_in') ) {
-        send_error("Not Logged in", 401);
+        send_error("Not logged in", 401);
     }
-
-    my $db = connect_db();
+ 
     my $sql = 'insert into entries (title, text) values (?, ?)';
-
-    my $sth = $db->prepare($sql) 
-        or die $db->errstr;
-
-    $sth->execute( 
+    my $sth = database->prepare($sql);
+    $sth->execute(
         body_parameters->get('title'),
-        body_parameters->get('text')   
-    ) or die $sth->srrstr;
-
-    set_flash('new entry posted!');
+        body_parameters->get('text')
+    );
+ 
+    set_flash('New entry posted!');
     redirect '/';
 };
-
-#login route
+ 
 any ['get', 'post'] => '/login' => sub {
-    my $err; 
-
+    my $err;
+ 
     if ( request->method() eq "POST" ) {
-        #process form input
-        if( body_parameters->('username') ne setting('username') ) {
-            $err = "Invalid username";  
-        } elsif ( body_parameters->get('password') ne setting('username') ) {
+        # process form input
+        if ( params->{'username'} ne setting('username') ) {
+            $err = "Invalid username";
+        }
+        elsif ( params->{'password'} ne setting('password') ) {
             $err = "Invalid password";
-        } else { 
+        }
+        else {
             session 'logged_in' => true;
-            set_flash('You are logged in');
+            set_flash('You are logged in.');
             return redirect '/';
         }
     }
-
-    #display login form
+ 
+    # display login form
     template 'login.tt', {
         err => $err,
     };
+ 
 };
-
-get '/logout' => sub{
-    app->destroy_session;  #remove a stored session 
-    set_flash('You are logged out,');
-    redirect '/'; #route th e user nabck to index page
+ 
+get '/logout' => sub {
+    app->destroy_session;
+    set_flash('You are logged out.');
+    redirect '/';
 };
-
-init_db();
-start;
+ 
+true;
